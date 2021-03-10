b63997d5f3d3d928f5d10e38fd7a0326fd16a203
Андрей <semenov@34web.ru>
2021-03-10 23:52:05 +0300

    Практика 5

---
+++ GeekBrainsPractice/functions.h
@@ -0,0 +1,99 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+
+// Функция-разделитель вывода задач
+void taskSeparator()
+{
+    cout << "\n---------------------------------------------------------------------\n\n";
+}
+
+
+/* 
+* @brief Рандомайзер чисел типа double
+* @param fMin - минимальное значение случайного числа
+* @param fMax - максимальное значение случайного числа
+*/
+double dRand(double fMin = -100, double fMax = 100)
+{
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+
+
+/* 
+* @brief Рандомайзер чисел типа int
+* @param fMin - минимальное значение случайного числа
+* @param fMax - максимальное значение случайного числа
+*/
+int iRand(int iMin = -100, int iMax = 100)
+{
+    return (rand() % (iMax * 2)) - iMin;
+}
+
+
+/*
+* @brief Заполнение массива случайными целыми числами
+* @param arr  - целочисленный массив
+* @param size - размер массива
+* @param min  - минимальное значение элемента массива
+* @param max  - максимальное значение элемента массива
+*/
+void fillArr(int* arr, size_t size, int min = -100, int max = 100)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = iRand(min, max);
+    }
+}
+
+/*
+* @brief Заполнение массива случайными дробными числами
+* @param arr  - массив дробных чисел
+* @param size - размер массива
+* @param min  - минимальное значение элемента массива
+* @param max  - максимальное значение элемента массива
+*/
+void fillArr(double* arr, size_t size, int min = -100, int max = 100)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = dRand(min, max);
+    }
+}
+
+/*
+* @brief Вывод целочисленного массива
+* @param arr  - массив целых чисел
+* @param size - размер массива
+*/
+void iArrOut(int* arr, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i];
+        if ((i + 1) < size)
+            cout << ",";
+        cout << " ";
+    }
+    cout << endl;
+}
+
+/*
+* @brief Вывод массива дробных чисел
+* @param arr  - массив дробных чисел
+* @param size - размер массива
+*/
+void dArrOut(double* arr, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i];
+        if ((i + 1) < size)
+            cout << ",";
+        cout << " ";
+    }
+    cout << endl;
+}
