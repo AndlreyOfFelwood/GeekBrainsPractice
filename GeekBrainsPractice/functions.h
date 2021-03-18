1eb78763d6c17460fe3689a87fceb433baf6a27c
Андрей <semenov@34web.ru>
2021-03-18 22:38:37 +0300

    Практика 7

--- GeekBrainsPractice/functions.h
+++ GeekBrainsPractice/functions.h
@@ -3,97 +3,101 @@
 
 using namespace std;
 
- 
-// Функция-разделитель вывода задач
-void taskSeparator()
+namespace MyFunc
 {
-    cout << "\n---------------------------------------------------------------------\n\n";
-}
+	// Функция-разделитель вывода задач
+	void taskSeparator();
 
 
-/* 
-* @brief – Рандомайзер чисел типа double
-* @param fMin - минимальное значение случайного числа
-* @param fMax - максимальное значение случайного числа
-*/
-double myRand(double fMin = -100, double fMax = 100)
-{
-    double f = (double)rand() / RAND_MAX;
-    return fMin + f * (fMax - fMin);
-}
+	/*
+	* @brief – Рандомайзер чисел типа int
+	* @param fMin - минимальное значение случайного числа
+	* @param fMax - максимальное значение случайного числа
+	*/
+	int myRand(int iMin = -100, int iMax = 100);
 
 
-/* 
-* @brief – Рандомайзер чисел типа int
-* @param fMin - минимальное значение случайного числа
-* @param fMax - максимальное значение случайного числа
-*/
-int myRand(int iMin = -100, int iMax = 100)
-{
-    return (rand() % (iMax * 2)) - abs(iMin);
-}
+	/*
+	* @brief – Рандомайзер чисел типа float
+	* @param fMin - минимальное значение случайного числа
+	* @param fMax - максимальное значение случайного числа
+	*/
+	float myRand(float fMin = -100, float fMax = 100);
 
 
-/*
-* @brief Заполнение массива случайными целыми числами
-* @param arr  - целочисленный массив
-* @param size - размер массива
-* @param min  - минимальное значение элемента массива
-* @param max  - максимальное значение элемента массива
-*/
-void fillArr(int* arr, size_t size, int min = -100, int max = 100)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        arr[i] = myRand(min, max);
-    }
-}
+	/*
+	* @brief – Рандомайзер чисел типа double
+	* @param fMin - минимальное значение случайного числа
+	* @param fMax - максимальное значение случайного числа
+	*/
+	double myRand(double fMin = -100, double fMax = 100);
 
-/*
-* @brief Заполнение массива случайными дробными числами
-* @param arr  - массив дробных чисел
-* @param size - размер массива
-* @param min  - минимальное значение элемента массива
-* @param max  - максимальное значение элемента массива
-*/
-void fillArr(double* arr, size_t size, int min = -100, int max = 100)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        arr[i] = myRand(min, max);
-    }
-}
 
-/*
-* @brief Вывод целочисленного массива
-* @param arr  - массив целых чисел
-* @param size - размер массива
-*/
-void arrOut(int* arr, size_t size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << arr[i];
-        if ((i + 1) < size)
-            cout << ",";
-        cout << " ";
-    }
-    cout << endl;
-}
+	/*
+	* @brief Заполнение массива случайными целыми числами
+	* @param arr  - целочисленный массив
+	* @param size - размер массива
+	* @param min  - минимальное значение элемента массива
+	* @param max  - максимальное значение элемента массива
+	*/
+	void fillArr(int* arr, size_t size, int min = -100, int max = 100);
 
-/*
-* @brief Вывод массива дробных чисел
-* @param arr  - массив дробных чисел
-* @param size - размер массива
-*/
-void arrOut(double* arr, size_t size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << arr[i];
-        if ((i + 1) < size)
-            cout << ",";
-        cout << " ";
-    }
-    cout << endl;
+	/*
+	* @brief Заполнение массива случайными дробными числами
+	* @param arr  - массив дробных чисел
+	* @param size - размер массива
+	* @param min  - минимальное значение элемента массива
+	* @param max  - максимальное значение элемента массива
+	*/
+	void fillArr(double* arr, size_t size, double min = -100, double max = 100);
+
+	/*
+	* @brief Заполнение массива случайными дробными числами
+	* @param arr  - массив дробных чисел
+	* @param size - размер массива
+	* @param min  - минимальное значение элемента массива
+	* @param max  - максимальное значение элемента массива
+	*/
+	void fillArr(float* arr, size_t size, float min = -100, float max = 100);
+
+	/*
+	* @brief Вывод целочисленного массива
+	* @param arr  - массив целых чисел
+	* @param size - размер массива
+	*/
+	void arrOut(int* arr, size_t size);
+
+	/*
+	* @brief Вывод массива дробных чисел
+	* @param arr  - массив дробных чисел
+	* @param size - размер массива
+	*/
+	void arrOut(double* arr, size_t size);
+
+	/*
+	* @brief Вывод массива дробных чисел
+	* @param arr  - массив дробных чисел
+	* @param size - размер массива
+	*/
+	void arrOut(float* arr, size_t size);
+
+	/*
+	* @brief Функция считает количество положительных и отрицательных элементов в массиве типа float
+	* @param arr - указатель на одномерный массив типа float
+	* @param size - размер массива
+	*/
+	void plusMinusCounter(float* arr, size_t size);
+
+	/*
+	* @brief Функция безопасного получения данных от пользователя
+	* @param var - ссылка на переменную в которую пользователь должен записать значение
+	*/
+	void cIn(int& var);
+
+	/*
+	* @brief Функция сортировки пузырьком
+	* @param arr - указатель на сортируемый массив
+	* @param size - размер сортируемого массива
+	*/
+	void arraySort(int* arr, size_t size);
 }
--- GeekBrainsPractice/main.cpp
+++ GeekBrainsPractice/main.cpp
@@ -5,212 +5,134 @@
 #include "functions.h"
 
 using namespace std;
+using namespace MyFunc;
 
-// Функции для первого задания
-size_t getSize(size_t size)
+#define CHECK_NUMBER(checkedNumber, maxNumber) (checkedNumber >= 0 && checkedNumber < maxNumber)
+#define SIZE 5
+
+//Структура для четвертого задания
+#pragma pack(push, 1)
+struct Emploee
 {
-    std::cin >> size;
-    if (size > 0)
-        return size;
-    else
-        getSize(size);
-}
+    string name = "";
+    unsigned short age = 0;
+    unsigned int salary = 0;
+};
+#pragma pack(pop)
 
-int* declareArray(int* ptrArr, size_t size)
-{
-    if (size > 0)
-    {
-        ptrArr = new (nothrow) int[size];
-
-        if (ptrArr != nullptr)
-            return ptrArr;
-        else
-            return 0;
-    }
-    else
-        return 0;
-}
-
-void initArray(int* arr, size_t size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        arr[i] = (int)pow(2, i);
-    }
-}
-
-// 1. Выделить в памяти динамический одномерный массив типа int. Размер массива запросить у пользователя. Инициализировать его числами степенями двойки: 1, 2, 4, 8, 16, 32, 64, 128 … Вывести массив на экран. Не забыть освободить память. =) Разбить программу на функции.
+// 1. Создайте проект из 2х cpp файлов и заголовочного (main.cpp, mylib.cpp, mylib.h) во втором модуле mylib объявить 3 функции: для инициализации массива (типа float), печати его на экран и подсчета количества отрицательных и положительных элементов. Вызывайте эти 3-и функции из main для работы с массивом.
 void firstTask()
 {
-    size_t size = 0;
+    cout << "Создайте проект из 2х cpp файлов и заголовочного (main.cpp, mylib.cpp, mylib.h) во втором модуле mylib объявить 3 функции: для инициализации массива (типа float), печати его на экран и подсчета количества отрицательных и положительных элементов. Вызывайте эти 3-и функции из main для работы с массивом.\n\n";
 
-    std::cout << "Введите размер массива: \n";
-    size = getSize(size);
+    int size = 0;
 
-    int* ptrArr = nullptr;
-    ptrArr = declareArray(ptrArr, size);
-
-    initArray(ptrArr, size);
-    arrOut(ptrArr, size);
-}
-
-
-//Функции для второго задания
-int** declareArray(int** ptrArr, size_t parentSize, size_t nestedSize)
-{
-    if (parentSize > 0 && nestedSize > 0)
+    do
     {
-        ptrArr = new int* [parentSize];
-        for (size_t i = 0; i < parentSize; i++) {
-            ptrArr[i] = new int[nestedSize];
-        }
-        return ptrArr;
-    }
-    return 0;
-}
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize> ::max(), '\n');
+        cout << "Введите размер массива\n";
+        cin >> size;
+    } while (size <= 0);
 
-void initArray(int** arr, size_t parentSize, size_t nestedSize)
-{
-    for (size_t i = 0; i < parentSize; i++)
+    float* ptrArr = new (nothrow) float[size];
+
+    if (ptrArr != nullptr)
     {
-        for (size_t j = 0; j < nestedSize; j++)
-        {
-            arr[i][j] = myRand(0, 100);
-        }
+        fillArr(ptrArr, size);
+        arrOut(ptrArr, size);
+        plusMinusCounter(ptrArr, size);
     }
+
+    delete[] ptrArr;
 }
 
-void arrOut(int** arr, size_t parentSize, size_t nestedSize)
-{
-    for (size_t i = 0; i < parentSize; i++)
-    {
-        for (size_t j = 0; j < nestedSize; j++)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
 
-// 2. Динамически выделить матрицу 4х4 типа int. Инициализировать ее псевдослучайными числами через функцию rand. Вывести на экран. Разбейте вашу программу на функции которые вызываются из main.
+// 2. Описать макрокоманду (через директиву define), проверяющую, входит ли переданное ей число (введенное с клавиатуры) в диапазон от нуля (включительно) до переданного ей второго аргумента (исключительно) и возвращает true или false, вывести на экран «true» или «false».
 void secondTask()
 {
-    int** ptrArr = nullptr;
-    size_t parentSize = 0;
-    size_t nestedSize = 0;
+    cout << "Описать макрокоманду (через директиву define), проверяющую, входит ли переданное ей число (введенное с клавиатуры) в диапазон от нуля (включительно) до переданного ей второго аргумента (исключительно) и возвращает true или false, вывести на экран «true» или «false».\n\n";
+    int checkedNumber, maxNumber = 50;
 
-    std::cout << "Введите размер родительского массива: \n";
-    parentSize = getSize(parentSize);
+    cIn(checkedNumber);
 
-    std::cout << "Введите размер вложенного массива: \n";
-    nestedSize = getSize(nestedSize);
-
-    ptrArr = declareArray(ptrArr, parentSize, nestedSize);
-
-    initArray(ptrArr, parentSize, nestedSize);
-    arrOut(ptrArr, parentSize, nestedSize);
+    CHECK_NUMBER(checkedNumber, maxNumber) == 1 ?
+        cout << "True\n" :
+        cout << "False\n";
 }
 
-//Функции для третьего задания
-string getName()
-{
-    string name;
-    cout << "Введите имя файла:\n";
-    cin >> name;
-
-    return name;
-}
-
-void createFile(string name, short num = 1)
-{
-    string fileName = name + ".txt";
-    ofstream fout(fileName);
-
-    if (num == 1)
-    {
-        fout << "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n In rhoncus placerat luctus. Aliquam erat volutpat.\n Aliquam malesuada turpis justo, nec porta risus pellentesque viverra. \nDuis laoreet neque augue, ac tristique odio semper ac.\n Phasellus non convallis quam, in porta justo.\n Praesent vulputate quis turpis quis rhoncus.\n Aenean feugiat lectus non viverra pulvinar.\n Sed euismod, metus ac vestibulum aliquam, lacus orci.\n";
-    }
-    else
-        fout << "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pharetra ligula augue, vitae ullamcorper lorem hendrerit ac. Ut finibus finibus ipsum ut luctus. Vivamus ac enim non felis mattis feugiat sit amet id erat. Nam varius elementum auctor. Suspendisse id urna sed urna vestibulum ullamcorper a et justo. Nulla sollicitudin velit quam, id tempus risus ultricies quis. Aliquam sollicitudin quis lectus commodo tristique. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus tristique quam sit amet turpis vulputate iaculis. Aliquam porta dapibus tortor vitae faucibus. Nulla molestie scelerisque tellus, at cursus urna facilisis et. Suspendisse.\n";
-
-    fout.close();
-}
-
-// 3. Написать программу c 2-я функциями, которая создаст два текстовых файла (*.txt), примерно по 50-100 символов в каждом (особого значения не имеет с каким именно содержимым). Имена файлов запросить у польлзователя.
+// 3. Задайте массив типа int. Пусть его размер задается через директиву препроцессора #define. Инициализируйте его через ввод с клавиатуры. Напишите для него свою функцию сортировки (например Пузырьком). Реализуйте перестановку элементов как макрокоманду SwapINT(a, b). Вызывайте ее из цикла сортировки.
 void thirdTask()
 {
-    string  firstFileName = getName(), 
-            secondFileName = getName();
+    cout << "Задайте массив типа int. Пусть его размер задается через директиву препроцессора #define. Инициализируйте его через ввод с клавиатуры. Напишите для него свою функцию сортировки (например Пузырьком). Реализуйте перестановку элементов как макрокоманду SwapINT(a, b). Вызывайте ее из цикла сортировки.\n\n";
 
-    createFile(firstFileName);
-    createFile(secondFileName, 2);
-}
+    int arr[SIZE];
 
-//Функции для четвертого задания
-string getFileData(string fileName)
-{
-    string resString = "";
-    ifstream fileData(fileName + ".txt");
-
-    if (fileData.is_open())
+    for (size_t i = 0; i < SIZE; i++)
     {
-        const size_t size = 255;
-        char buf[size];
-        while (!fileData.eof())
-        {
-            fileData.getline(buf, size);
-            resString += buf;
-        }
-        fileData.close();
-        return resString;
+        cout << "Введите " << i << " элемент массива\n";
+        cIn(arr[i]);
     }
-    else
-    {
-        cout << "Error. Can not open file "+ fileName +".";
-        return "";
-    }
+
+    cout << "\nМассив до сортировки:\n\n";
+    arrOut(arr, SIZE);
+
+    arraySort(arr, SIZE);
+
+    cout << "\nМассив после сортировки:\n\n";
+    arrOut(arr, SIZE);
 }
 
-void createFile(string name, string data)
+//Функция для четвертого задания
+void structOut(Emploee* emploee)
 {
-    string fileName = name + ".txt";
-    ofstream fout(fileName);
-
-    fout << data;
-
-    fout.close();
+    cout << "Профайл работника:\n\n";
+    cout << "Имя: ";
+    cout << emploee->name << endl;
+    cout << "Возраст: ";
+    cout << emploee->age << endl;
+    cout << "Зарплата: ";
+    cout << emploee->salary << endl;
 }
 
-// 4. * Написать функцию, «склеивающую» эти файлы в третий текстовый файл (имя файлов спросить у пользователя).
+// 4. * Объявить структуру Сотрудник с различными полями. Сделайте для нее побайтовое выравнивание с помощью директивы pragma pack. Выделите динамически переменную этого типа. Инициализируйте ее. Выведите ее на экран и ее размер с помощью sizeof. Сохраните эту структуру в текстовый файл.
 void fourthTask()
 {
-    string  firstFileData, secondFileData;
+    cout << "\n* Объявить структуру Сотрудник с различными полями. Сделайте для нее побайтовое выравнивание с помощью директивы pragma pack. Выделите динамически переменную этого типа. Инициализируйте ее. Выведите ее на экран и ее размер с помощью sizeof. Сохраните эту структуру в текстовый файл.\n\n";
 
-    firstFileData = getFileData(getName());
-    secondFileData = getFileData(getName());
 
-    createFile(getName(), firstFileData + secondFileData);
+
+    Emploee* emploee = new (nothrow) Emploee;
+
+    emploee->name = "Иванов Иван Иванович";
+    emploee->age = 33;
+    emploee->salary = 77'777;
+
+    structOut(emploee);
+
+    cout << "\nРазмер структуры: ";
+    cout << sizeof(emploee) << endl;
+    
+    ofstream fout("emploee.txt");
+    
+    fout << "Профайл работника:\n\n";
+    fout << "Имя: ";
+    fout << emploee->name << endl;
+    fout << "Возраст: ";
+    fout << emploee->age << endl;
+    fout << "Зарплата: ";
+    fout << emploee->salary << endl;
+
+    fout.close();
+
+    delete emploee;
 }
 
 
-// 5. * Написать программу, которая проверяет присутствует ли указанное пользователем при запуске программы слово в указанном пользователем файле (для простоты работаем только с латиницей).
+// 5. * Сделайте задание 1 добавив свой неймспейс во втором модуле (первое задание тогда можно не делать)
 void fifthTask()
 {
-    string searchWord, fileName, fileData;
-
-    cout << "Введите искомое слово:\n";
-    cin >> searchWord;
-
-    cout << "Введите название файла, в котором нужно найти данное слово:\n";
-    cin >> fileName;
-
-    fileData = getFileData(fileName);
-    (fileData.find(searchWord) == -1) ?
-        cout << "Слово " + searchWord + " не найдено в файле " + fileName << endl
-        :
-        cout << "В файле " + fileName + " присутствует слово " + searchWord << endl;
-
+    firstTask();
 }
 
 
